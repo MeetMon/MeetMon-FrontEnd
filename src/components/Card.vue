b1cdd0ef1e6b6e2d59344541453f9f796166d27b
paithoa <handy.hasan@yahoo.com>
2018-08-21 16:53:54 +1000

    added some emoji to the front

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,14 +7,27 @@
     <md-card-content>
       <h2 class="md-subhead">{{description}}</h2>
     </md-card-content>
+    <VueEmoji @input="onInput" :value="myText" width="100px" height="50px" />
+
   </md-card>
 </template>
 
 <script>
+import VueEmoji from 'emoji-vue';
+
 export default {
+  methods: {
+    onInput(event) {
+            //event.data contains the value of the textarea
+        },
+      },
+  components: {
+           VueEmoji
+         },
   name: 'card',
   props: ['image', 'title', 'description'],
 };
+
 </script>
 
 <style>
