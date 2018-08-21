fb5803c5032cc18526a24d89f497c8d733376684
paithoa <handy.hasan@yahoo.com>
2018-08-21 19:25:02 +1000

    yes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,21 +8,32 @@
       <h2 class="md-subhead">{{description}}</h2>
     </md-card-content>
     <VueEmoji @input="onInput" :value="myText" width="100px" height="50px" />
-
+    <vue-star animate="animated bounceIn" color="#F05654">
+      <img slot="icon" src="../assets/like.png" />
+    </vue-star>
+    <vue-star animate="animated rubberBand" color="#F05654">
+    <a slot="icon" class="fa fa-heart" @click="handleClick"></a>
+  </vue-star>
   </md-card>
 </template>
 
 <script>
 import VueEmoji from 'emoji-vue';
+import VueStar from 'vue-star'
+
 
 export default {
   methods: {
     onInput(event) {
             //event.data contains the value of the textarea
         },
+        handleClick () {
+     //do something
+   },
       },
   components: {
-           VueEmoji
+           VueEmoji,
+           VueStar
          },
   name: 'card',
   props: ['image', 'title', 'description'],
